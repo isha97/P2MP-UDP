bb9daf1aadb8df4e414020366052c6f91d2faa20
isha97 <tarteisha@gmail.com>
2016-09-17 22:07:21 +0530

    Modified

--- Client.py
+++ Client.py
@@ -1,114 +1,167 @@
+""" Implementation of the Client side of UDP File Transfer using Stop and Wait ARQ """
+
+""" Buffer size of client should be smaller than the Server's. Otherwise data loss happens  """
+
 from socket import *
 import sys
 import os
 import math
 import time
 
-
+""" counts the number of digits in a given number """
 def no_of_digits(a):
-	count=0
-	while(a!=0):
-		a=a/10
-		count=count+1
-	return count
+    count=0
+    while(a!=0):
+        a=a/10
+        count=count+1
+    return count
 
 
+""" input : sequence number, output : 10 digit long formatted sequence number padded with zeroes"""
 def get_seq_no(a):
-	if(a==0):
-		return '0'*10
-	count=no_of_digits(a)
-	no_of_zeroes=10-count
-	val='0'*no_of_zeroes + '%d' %a
-	return val
+    if(a==0):
+        return '0'*10
+    count=no_of_digits(a)
+    no_of_zeroes=10-count
+    val='0'*no_of_zeroes + '%d' %a
+    return val
 
-# Buffer size of client should be smaller than the Server's. Otherwise data loss happens
+""" creates a datagram(UDP) socket with ipv4 addressing """
 s=socket(AF_INET,SOCK_DGRAM)
+
+""" stores the number of command line arguments """
 no_of_arg=len(sys.argv)
+
+""" MSS stands for maximum segment size, file can be of any file-format, but should be in the current directory  """
 if(no_of_arg!=4):
-	print "expected format python %s <number of hosts> <filename> <MSS> "%(sys.argv[0])
+    print "expected format python %s <number of hosts> <filename> <MSS> "%(sys.argv[0])
+
+    """ 89 bytes are required for sequence number and checksum as python stores metadata as well """
+
 elif int(sys.argv[3])<89 or int(sys.argv[3])>2048:
-	print sys.agrv[2]," should be greater than 89 bytes and lesser than 2048 bytes"
+    print sys.agrv[2]," should be greater than 89 bytes and lesser than 2048 bytes"
+
 
 else:
-	host=[]
-	addr=[]
-	port=[]
-	no_of_hosts=int(sys.argv[1])
-	for i in range(no_of_hosts):
-		host.append(raw_input("Enter hostname %d " %i))
-		port.append(raw_input("Enter port of %d th host" %i))
-		port[i]=int(port[i])
-		addr.append((host[i],port[i]))
+    host=[]
+    """ addr is a list of the tuples (host_ip,port_no) """
+    addr=[]
+    port=[]
+    no_of_hosts=int(sys.argv[1])
+    """ Inputs host ips and port numbers one after the other """
+    for i in range(no_of_hosts):
+        host.append(raw_input("Enter hostname %d " %i))
+        port.append(raw_input("Enter port of %d th host" %i))
+        port[i]=int(port[i])
+        addr.append((host[i],port[i]))
 
-	
-		
-	#addr=(host,port)
-	t0=time.time()
-	buff_size=int(sys.argv[3])-47-42
-	
-	file_name=sys.argv[2]
-	file_size=os.stat(file_name).st_size
-	print file_size
-	seq_no=int(math.ceil(float(file_size)/buff_size))
-	print "%d" %seq_no
-	for i in range(no_of_hosts):
-		while(s.sendto("%d" %seq_no,addr[i])):
-			break
-	a=addr[:]
-	while(len(a)!=0):
-		d,ad=s.recvfrom(buff_size)
-		a.remove(ad)
+    
+    """ t0 stores the starting time of the file transfer to the servers """
+    t0=time.time()
 
+    """ buff_size is the number of bytes of data read at a time from the file, 47 bytes for sequence no, 42 for checksum """
+    buff_size=int(sys.argv[3])-47-42
+    
+    """ filename is obtained as command line argument """
+    file_name=sys.argv[2]
 
-	seq=0
-	curr_seq=get_seq_no(seq)
-	f=open(file_name,'rb')
-	data=f.read(buff_size)
-	#print data
-	s.settimeout(0.5)
-	
-	while(data):
-		
-		a=addr[:]
-		data=curr_seq+data
-		ascii_val=map(ord,data)
-		group_val=[]
-		for i in range(1,len(ascii_val)):
-			group_val.append(ascii_val[i-1]*256+ascii_val[i])
-		checksum=reduce(lambda x,y:(x+y)%65536,group_val)
-		checksum=checksum^65535
-		chksum_string='0'*5+'%d' %checksum
-		chksum=chksum_string[-5:]
-		#print chksum
-		data=data+chksum
-		host_resp=0
-		while(len(a) is not 0):
-			#print "curr seq no is ",curr_seq
-			for i in range(len(a)):
-				while(s.sendto(data,a[i])):
-					break
-			
-			while(host_resp<no_of_hosts):
-				#print "waiting for ack.."
-				try:
-					ack,ad=s.recvfrom(buff_size)
-					#print "ack  = ",ack
-					#if(int(curr_seq) is int(ack)):
-					host_resp=host_resp+1
-				       	a.remove(ad)
-				except:
-					print "timeout, Sequence number = ",curr_seq
-					break
-			
-			#print t
+    """ file_size contains total file size in bytes """
+    file_size=os.stat(file_name).st_size
+    print "File size of ",file_name," is ",file_size
+    """ calculates the total number of packets that will be sent """
+    seq_no=int(math.ceil(float(file_size)/buff_size))
+    print "Total number of packets that will be sent is %d" %seq_no
+    """ The first UDP packet will contain the information about the number of packets """
+    for i in range(no_of_hosts):
+        while(s.sendto("%d" %seq_no,addr[i])):
+            break
+    a=addr[:]
+
+    """ this loop will run until ACKs are received from all the servers """
+    while(len(a)!=0):
+        d,ad=s.recvfrom(buff_size)
+        a.remove(ad)
 
 
-			
-		data=f.read(buff_size)
-		seq=seq+1
-		curr_seq=get_seq_no(seq)
-	total_time=time.time()-t0
-	print "Total time is %d"%total_time		
-	s.close()
-	f.close()
+    seq=0
+    curr_seq=get_seq_no(seq)
+    """ file is read in binary format """
+    f=open(file_name,'rb')
+
+    data=f.read(buff_size)
+    
+    """ If within 0.5s ACKs are not received, it will be resent to the resepctive hosts """
+    s.settimeout(0.5)
+    
+    """ this loop runs as long as there EOF is not reached """
+    while(data):
+        
+        a=addr[:]
+        data=curr_seq+data
+        ascii_val=map(ord,data)
+        group_val=[]
+        
+        """ groups 16bits together(2 ASCII characters) """
+        for i in range(1,len(ascii_val)):
+            group_val.append(ascii_val[i-1]*256+ascii_val[i])
+         
+        """ adds all the 16bit values together and obtains the 16LSBs and ignores the rest"""
+        checksum=reduce(lambda x,y:(x+y)%65536,group_val)
+
+        """ compliments this value to obtain UDP checksum """
+        checksum=checksum^65535
+
+        """ checksum is made 5 digits long and is appended with data """
+        chksum_string='0'*5+'%d' %checksum
+        chksum=chksum_string[-5:]
+    
+        data=data+chksum
+    
+        host_resp=0
+        """ a is a list of all addresses. it becomes empty when the packet reaches all hosts """
+        """ one of the following might occur when a packet is sent
+        1. All hosts might receive the packets and send ACKs within the timeout period.
+        2. Few hosts might not receive the packets. Hence packet to those hosts needs to be resent after 0.5s
+        3. Few hosts might receive the packet but ACK might fail, in which case the client will resend the packet"""
+        while(len(a) is not 0):
+            #print "curr seq no is ",curr_seq
+
+            """ packet is sent to all hosts in an iterative fashion """
+            for i in range(len(a)):
+                while(s.sendto(data,a[i])):
+                    break
+            
+            """ the client waits for 0.5s to receive all ACKS. """
+          
+            while(host_resp<no_of_hosts):
+                """ socket.Timeouterror occurs when timeout period is elapsed  """
+                try:
+                    ack,ad=s.recvfrom(buff_size)
+                    
+                    """ if out-of-sequence packet's ACK is obtained, it should be ignored """
+                    if(int(curr_seq) == int(ack)):#understand 'is' functionality
+                        host_resp=host_resp+1
+                        a.remove(ad)
+        
+                except:
+                    print "timeout, Sequence number = ",curr_seq
+                    break
+            
+
+
+        """ more data is read from file """
+        data=f.read(buff_size)
+
+        """ sequence numbers is incremented """
+        seq=seq+1
+        curr_seq=get_seq_no(seq)
+    
+    """ Calculates the total time for UDP transfer """
+    total_time=time.time()-t0
+    print "Total time is %d"%total_time        
+    
+    """ close the socket """
+    s.close()
+    """ close the file """
+    f.close()
 
--- Server.py
+++ Server.py
@@ -1,97 +1,128 @@
+""" Implementation of the Server side of the Point to Multipoint file tranfer using Stop and Wait ARQ"""
+
 from socket import *
 import sys
 import random
 
-
+""" Stores the number of command line arguments"""
 no_of_arg=len(sys.argv)
 
-
+""" probability refers to the probability loss service number """
 if(no_of_arg!=4):
         print "expected format python %s <port no> <filename> <probability>" %(sys.argv[0])
-
-host="10.42.0.42"
+	sys.exit(1)
+""" specifies the IP of the server, hence the Network Interface is also known """
+host="127.0.0.1"
+""" Listening port of the server """
 port=int(sys.argv[1])
+
+
 randomList=[]
 
+""" A datagram(UDP) socket is created and suports ipv4 addressing"""
 s=socket(AF_INET,SOCK_DGRAM)
 prob = float(sys.argv[3]);
+
+""" contains the source host address and port number as a tuple """
 addr=(host,port)
+
+""" the server binds to this socket and waits for hosts """
 s.bind(addr)
+""" the input buffer size is fixed to 2048 bytes """
 buff_size=2048
 
+
+""" once the first packet regarding Seq No is sent, ACKs are sent back to the client"""
 msg = 'sequence no. recieved!'
 data,address=s.recvfrom(buff_size)
 s.sendto(msg,address)
 
+
 file_name=sys.argv[2]
+""" A new file is open or an existing file is rewritten"""
 f=open(file_name,'wb')
-#print "The total sequences are :  "
-print int(data)
 total = int(data)
 
-
+""" though UDP is unreliable, very few packets are lost in reality. hence a few packets are made to be lost on purpose """
+""" the probabilty of losing packets is taken as a command line input. and the losing-packets numbers are generated randomly and stored in randomList"""
 prob = int(prob*total)
 for i in range(prob):
-	gen_no=random.randint(0,total-1)	
-	"""while(gen_no in randomList):
-		gen_no=random.randint(0,total-1)		"""
-	randomList.append(gen_no)		
-#print len(randomList)
+    gen_no=random.randint(0,total-1)    
+    randomList.append(gen_no)        
+
 randomList.sort()
 print randomList
 
 
 try:
-	data,address=s.recvfrom(buff_size)
-	
-	count =0	
-	while(True):
-		
-		if(count not in randomList):
-			#print "Sequence Number", ack			
-			ack=data[0:10]
-			ack_no=int(ack)
-			#print "seq no is %d"%ack_no
-			#print "count is %d"%count
-			if(ack_no<count):
-				while(not s.sendto(ack,address)):
-		       			continue
-				count = count-1
-				print "	ack < count"
-			elif(ack_no==count):
-				ascii_val=map(ord,data[:-5])
-				group_val=[]
-				for i in range(1,len(ascii_val)):
-					group_val.append(ascii_val[i-1]*256+ascii_val[i])
-				
-				checksum=reduce(lambda x,y:(x+y)%65536,group_val)
-				chk = data[-5:]
-				checksum_calculate=(checksum+int(chk))%65535
-				#print checksum_calculate
-				if(checksum_calculate==0):
-		       			while(not s.sendto(ack,address)):
-		       				continue
-					#print ack, "sent"
-		       			f.write(data[10:-5])
-				else:
-					print "checksum didn't match!"
-					count =count-1
-			else:
-				print "Sequence no didn't match"
-				count = count-1
-			
-		else:
-			print "Packet Loss, Sequence number = %d "%count			
-			randomList.remove(count)
-			count =count-1
-		if(count == total-1):
-			#print count is total-1
-			raise ValueError()
-		data,address=s.recvfrom(buff_size)
-		
-		count = count+1
-except:
-	f.close()
-	print "File obtained"	
+    """ blocked until it receives data from the client """
+    data,address=s.recvfrom(buff_size)
+    
+    """ count keeps track of the number of packets received """
+    count =0    
+    while(True):
+        """ if the packet number is not in randomList, it has to be accepted  """
+        if(count not in randomList):
+                       
+            ack=data[0:10]
+            ack_no=int(ack)
+            """ if the received packet has already been received, it means ACK might have failed. Hence ACK is resent"""
+            if(ack_no<count):
+                while(not s.sendto(ack,address)):
+                           continue
+                count = count-1
+                print "    ack < count "
+            
+            elif(ack_no==count):
+                
+                """ if the packet obtained is in-sequence, then checksum has to be verified"""
+            
+                
+                ascii_val=map(ord,data[:-5])
+                group_val=[]
+                for i in range(1,len(ascii_val)):
+                    group_val.append(ascii_val[i-1]*256+ascii_val[i])
+                
+                checksum=reduce(lambda x,y:(x+y)%65536,group_val)
+                chk = data[-5:]
+                checksum_calculate=(checksum+int(chk))%65535
 
+                """ if the calculated checksum and the received checksums complement each other, then the packet is said to be error free. Hence ACK is sent """
+                if(checksum_calculate==0):
+                           while(not s.sendto(ack,address)):
+                               continue
+                           f.write(data[10:-5])
+                else:
+                    """ if checksums don't match, ACKs are not sent. Hence packet will be resent """
+               
+                    print "checksum didn't match!"
+                    count =count-1
+
+            else:
+                """ this is a condition that never occurs in a Stop and Wait ARQ as only when ACKs for a packet is obtained, the next packet is sent """
+            
+                print "Sequence no didn't match"
+                count = count-1
+        else:
+            """ when the packet number is in randomList, then the packet is rejected on purpose. Hence it will be resent"""
+        
+            print "Packet Loss, Sequence number = %d "%count            
+            """ Once the packet is rejected, it is removed from the list"""
+            randomList.remove(count)
+            count =count-1
+        """ when count is one less than total packet number, exception is raised as all packets are received."""
+        """ count is one less than total as seq_no begins from zero"""
+        if(count == total-1):
+            #print count is total-1
+            raise ValueError()
+        
+        """ read data from the input buffer """
+        data,address=s.recvfrom(buff_size)
+        count = count+1
+except:
+    """ once all packets are obtained, file is closed """
+    f.close()
+    print "File obtained"    
+
+""" socket is closed """
 s.close()
